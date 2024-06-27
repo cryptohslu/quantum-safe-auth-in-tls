e9ae90582e13e3c819e1cf384c0230eb2c48fd49
Iyán Méndez Veiga <me@iyanmv.com>
2024-06-27 10:10:41 +0200

    Use full path to binary ip
    subprocess.run() does not work correctly if sudo requires password. I
    think Joshua created an user where sudo could run anything without
    asking for password. We can, instead, allow ONLY /usr/bin/ip to be run
    without password. But for that the exact path must match, so let's use
    the full path instead of the executable name.

--- virt-test-env/namespace-cleanup.sh
+++ virt-test-env/namespace-cleanup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
+IP=$(command -v ip)
+
 # Starting
 echo "Start cleaning up..."
 
 # We check if ns1 and ns2 namespaces exist
-netns=$(ip netns list | grep "ns[12]" || [[ $? == 1 ]])
+netns=$($IP netns list | grep "ns[12]" || [[ $? == 1 ]])
 
 if [[ ${#netns} -eq 0 ]]; then
     echo "Nothing to clean. Exiting..."
@@ -13,15 +15,15 @@ if [[ ${#netns} -eq 0 ]]; then
 fi
 
 # Shutting down the veth devices
-sudo ip -n ns1 link set dev veth1 down
-sudo ip -n ns2 link set dev veth2 down
+sudo $IP -n ns1 link set dev veth1 down
+sudo $IP -n ns2 link set dev veth2 down
 
 # Delete the veth pair (this command will remove both veth1 and veth2)
-sudo sudo ip -n ns1 link delete veth1 type veth
+sudo $IP -n ns1 link delete veth1 type veth
 
 # Delete the two namespaces
-sudo ip netns del ns1
-sudo ip netns del ns2
+sudo $IP netns del ns1
+sudo $IP netns del ns2
 
 # Finished
 echo "Cleanup finished."
--- virt-test-env/namespace-setup.sh
+++ virt-test-env/namespace-setup.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 set -euo pipefail
 
+IP=$(command -v ip)
+
 # Starting
 echo "Start the setup..."
 
 # Set up two namespaces with the name ns1 and ns2
-sudo ip netns add ns1 
-sudo ip netns add ns2
+sudo $IP netns add ns1
+sudo $IP netns add ns2
 
 # Create a virtual Ethernet pair (veth101 and veth102) and link them to the namespaces (ns1 and ns2, respectively)
-sudo ip link add name veth1 address 00:00:00:00:00:01 netns ns1 type veth peer name veth2 address 00:00:00:00:00:02 netns ns2
+sudo $IP link add name veth1 address 00:00:00:00:00:01 netns ns1 type veth peer name veth2 address 00:00:00:00:00:02 netns ns2
 
 # Assign an IP address to each veth device and change the device state to "up"
-sudo ip -n ns1 addr add 10.5.0.1/24 dev veth1
-sudo ip -n ns1 link set dev veth1 up
-sudo ip -n ns2 addr add 10.6.0.1/24 dev veth2
-sudo ip -n ns2 link set dev veth2 up
+sudo $IP -n ns1 addr add 10.5.0.1/24 dev veth1
+sudo $IP -n ns1 link set dev veth1 up
+sudo $IP -n ns2 addr add 10.6.0.1/24 dev veth2
+sudo $IP -n ns2 link set dev veth2 up
 
 # Disable TCP Segmentation Offload (TSO), GSO and GRO
-sudo ip netns exec ns1 ethtool -K veth1 gso off gro off tso off
-sudo ip netns exec ns2 ethtool -K veth2 gso off gro off tso off
+sudo $IP netns exec ns1 ethtool -K veth1 gso off gro off tso off
+sudo $IP netns exec ns2 ethtool -K veth2 gso off gro off tso off
 
 # Finished.
 echo "Setup finished."
